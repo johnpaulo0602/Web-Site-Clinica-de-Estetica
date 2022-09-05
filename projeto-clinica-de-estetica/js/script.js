f00309247b9ea106ec2f73a4a272e751c54a07a7
John Paulo <johnpaulo0602@gmail.com>
2022-09-05 19:24:57 -0300

    function de alert de login e cadastro

--- projeto-clinica-de-estetica/js/script.js
+++ projeto-clinica-de-estetica/js/script.js
@@ -1,4 +1,4 @@
-// Inicio Carrossel
+// ==========================Inicio Carrossel
 
 let count = 1;
 document.getElementById("radio1").checked = true;
@@ -17,10 +17,12 @@ function nextimage(){
     document.getElementById("radio"+count).checked = true;
 }
 
-// Final carrossel
+//========================== Final carrossel
 
-// Inicio Login
+//========================== Inicio Login
 
+
+// Alternar entre o formulario de login e cadastro além de mudar a cor do botao
 let formEntrar = document.querySelector('#formEntrar')
 let formCadastro = document.querySelector('#formCadastro')
 let btnColor = document.querySelector('.btnColor')
@@ -39,15 +41,15 @@ document.querySelector('#btnCadastro')
         btnColor.style.left = "165px"
 })
 
-
+// função para mostrar o formulario de cadastro
 const showCadastro = document.querySelector("#showCadastro")
 const cadastroEmbrulho = document.querySelector('.cadastroEmbrulho')
-
 
 showCadastro.addEventListener('click', () => {
     cadastroEmbrulho.style.display = 'block'
 })
 
+// função para fechar o formulário quando clicado no x ou fora do formulario
 cadastroEmbrulho.addEventListener('click', event => {
     const classNameOfClickedElement = event.target.classList[0]
 
@@ -56,4 +58,28 @@ if (classNameOfClickedElement === 'fa-solid' || classNameOfClickedElement === 'c
 })
 
 
-// Final Login
+// Função para mostrar alert de login realizado
+
+function loginsuccess(){
+    let loginEmail = document.querySelector('#loginEmail').value;
+    let loginPassword = document.querySelector('#loginPassword').value;
+
+    if (loginEmail != "" && loginPassword != "") {
+        alert('Login realizado com sucesso!')
+    }
+}
+
+
+// função para mostrar alert de cadastro realizado
+function registersuccess(){
+    let registerEmail = document.querySelector('#registerEmail').value;
+    let registerPassword = document.querySelector('#registerPassword').value;
+    let registerPassword2 = document.querySelector('#registerPassword2').value;
+
+    if (registerEmail != "" && registerPassword != "" && registerPassword2 != "") {
+        alert('Cadastro realizado com sucesso!')
+    }
+}
+
+
+// ==========================Final Login
